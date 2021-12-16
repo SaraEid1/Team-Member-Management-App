a014a0df5c234500bf669d5a984e0f06dc48a606
Sara Eid <sara.eid15@stemmaadi.moe.edu.eg>
2021-12-16 03:53:26 -0800

    done

--- my_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-NUMS = [
-    (0, "Regular - Can't delete members"),
-    (1, "Admin - Can delete members"),
-    ]
-class CHOICES(forms.Form):
-    NUMS  = forms.CharField(widget=forms.RadioSelect(choices=NUMS))
